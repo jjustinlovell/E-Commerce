e3bd42643a5154c1486511291637b543a3357ed1
justin lovell <justinlvell18@gmail.com>
2024-06-02 12:19:33 +0700

    PART2

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -44,6 +44,90 @@ const specialOfferItems = [
 
 ];
 
+// const slides = document.querySelectorAll(".slides img");
+// let slideIndex = 0;
+// let intervalId = null;
+
+// document.addEventListener("DOMContentLoaded",initializeSlider);
+
+// function initializeSlider(){
+
+//     if(slides.length > 0){
+//         slides[slideIndex].classList.add("displaySlide");
+//         intervalId = setInterval(nextSlide, 5000)
+//     }
+    
+
+// }
+
+// function showSlide(index){
+
+//     if(index >= slides.length){
+//         slideIndex = 0;
+//     }
+//     else if(index < 0)
+//     {
+//         slideIndex = slides.length - 1;
+//     }
+
+//     slides.forEach(slide => {
+//         slide.classList.remove("displaySlide")
+//     });
+
+//     slides[slideIndex].classList.add("displaySlide");
+
+
+// }
+
+// function previousSlide(){
+//     clearInterval(intervalId);
+//     slideIndex--;
+//     showSlide(slideIndex);
+// }
+
+// function nextSlide(){
+//     slideIndex++;
+//     showSlide(slideIndex);
+// }
+
+const slides = document.querySelectorAll(".slides .slide");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function previousSlide() {
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
 const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
@@ -97,7 +181,7 @@ const createSpecialOfferItem = (item) => {
 
 
     const button = document.createElement('button');
-    button.textContent = 'Add to Cart';
+    button.textContent = 'Add';
     button.className = 'add-to-cart';
     button.setAttribute('data-title', item.title);
     button.setAttribute('data-price', item.price);
